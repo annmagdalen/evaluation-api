55ec1e210e9840bfe8b846a1fdfbe785bc1c39f4
annmagdalen <annmagdalen@hotmail.com>
2017-06-26 19:40:54 +0200

    Added batches to seeds

--- src/seeds.js
+++ src/seeds.js
@@ -10,6 +10,24 @@ const user = {
   password: 'qwerty1'
 };
 
+const batches = [
+  {
+    number: 1,
+    startDate: 15-01-2017,
+    endDate: 14-03-2017
+  },
+  {
+    number: 2,
+    startDate: 15-03-2017,
+    endDate: 14-05-2017
+  },
+  {
+    number: 3,
+    startDate: 15-05-2017,
+    endDate: 14-07-2017
+  }
+]
+
 const feathersClient = feathers();
 
 feathersClient
@@ -24,6 +42,18 @@ feathersClient.service('users').create(user)
     email: user.email,
     password: user.password
   })
+  .then(() => {
+    batches.map((batch) => {
+      feathersClient.service('batches').create(batch)
+        .then((result) => {
+        }).catch((error) => {
+          console.error('Error seeding recipe!', error.message);
+        });
+    })
+  })
+  .catch(function(error){
+    console.error('Error authenticating!', error);
+  });
 })
 .catch(function(error) {
   console.error('Error creating user!');
